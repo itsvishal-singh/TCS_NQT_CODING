239d3253ca3b4ae9dd66cc5856dc6e0e4633cbfc
Vishal <vishal.200087@gmail.com>
2026-02-10 10:59:38 +0530

    A Small Step to TCS

--- src/Arrays/LinearSearch.java
+++ src/Arrays/LinearSearch.java
@@ -1,7 +1,20 @@
 package Arrays;
 
+
 public class LinearSearch {
     public static void main(String[] args) {
+        int[] arr = {3,5,9,12,56,95,32,51};
+        int x = 9;
+        boolean flag = false; // nahi mila
+        for(int no : arr){
+            if(no == x){
+                flag = true;
+                break;
+            }
+        }
+        if(flag==false) // (!flag)
+            System.out.println("Nahi Mila");
+        else System.out.println("Mil Gyaa");
 
     }
 }
